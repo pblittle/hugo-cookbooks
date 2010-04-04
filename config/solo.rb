9d1fa5daf0fa19d462e58db3b412ba3324d22e5a
P. Barrett Little <barrett@designercode.com>
2010-04-04 01:57:11 -0400

    Moved cookbooks path to the beginning of the cookbook_path array. Site-cookbooks wouldn't run in 0.8.x without this tweak.

--- config/solo.rb
+++ config/solo.rb
@@ -1,7 +1,7 @@
-log_level          :info
+log_level          :debug
 log_location       STDOUT
 file_cache_path File.join(File.dirname(__FILE__), '..')
-cookbook_path [ File.join(File.dirname(__FILE__), '..', "site-cookbooks"), File.join(File.dirname(__FILE__), '..', "cookbooks") ]
+cookbook_path [ File.join(File.dirname(__FILE__), '..', "cookbooks"), File.join(File.dirname(__FILE__), '..', "site-cookbooks") ]
 role_path File.join(File.dirname(__FILE__), '..', "roles")
 ssl_verify_mode    :verify_none
 Chef::Log::Formatter.show_time = false
